6d2adb53cf429f1c808bea6681b42f879ae78f11
wesleysantos20y <pandanoob76@gmail.com>
2025-04-10 12:31:48 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("pizzas.csv")
+
+modelo = LinearRegression()
+x = df[["diametro"]]
+y = df[["preco"]]
+
+st.title("projeto1")
