9d02058b9dcae395fa81b68c3051b27f85031b4e
wesleysantos20y <pandanoob76@gmail.com>
2025-04-10 12:49:20 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,4 +8,13 @@ modelo = LinearRegression()
 x = df[["diametro"]]
 y = df[["preco"]]
 
+modelo.fit(x, y)
+
 st.title("projeto1")
+st.divider()
+
+diamentro = st.number_input("digite o tamanho do diamentro da pizza: ")
+
+if diamentro:
+    preco_previsto = modelo.predict([[diamentro]])[0][0]
+    st.write(f"o valor da pizza com diamentro de {diamentro} e de R${preco_previsto}.")
